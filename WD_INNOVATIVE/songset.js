9c1aa97c9a7372877cb676306f43bf5f3f2a186a
PervezAhmed <vvit20bq1a05i8@gmail.com>
2023-01-29 19:57:00 +0530

    Changes done in Songset.js

--- WD_INNOVATIVE/songset.js
+++ WD_INNOVATIVE/songset.js
@@ -1,101 +1,25 @@
-var songrun=false;
-var count=1;
-var mod=1;
-var path=["songs\\ban ja rani.mp3"
-,"songs\\Banduk meri laila.mp3"
-,"songs\\barish.mp3"
-,"songs\\haareya.mp3"
-,"songs\\ik vari aa.mp3"
-,"songs\\main tera.mp3"
-,"songs\\mercy.mp3"
-,"songs\\musafir.mp3"
-,"songs\\o sathi.mp3"
-,"songs\\phir bhi.mp3"];
+// path of songs folder of each expresion
+folderspath=["songs\\Angry\\","songs\\Happy\\","songs\\Sad\\","songs\\Neutral\\"];
 
-var sname=["Ban Ja tu meri Rani",
-"Banduk Meri Laila",
-"Barish",
-"Haareya",
-"Ik vari aa",
-"main tera boyfriend",
-"mercy",
-"musafir",
-"o sathi",
-"Phir Bhi"
-];
-
-var sd=["Artist: Guru Randhawa<br>Movie: Tumhari Sulu<br>Released: 2017",
-"Artists: Ash King, Jigar Saraiya<br>Featured artists: Sidharth Malhotra, Raftaar<br>Movie: A Gentleman<br>Released: 2017"
-,"Artists: Ash King, Shashaa Tirupati<br>Movie: Half Girlfriend<br>Released: 2017<br>Awards: Zee Cine Award for Song of the Year"
-,"Artist: Arijit Singh<br>Movie: Meri Pyaari Bindu<br>Released: 2017<br>Nominations: Mirchi Music Awards for Best Song Producer - Programming & Arranging"
-,"Artist: Arijit Singh<br>Movie: Raabta<br>Released: 2017"
-,"Artists: Arijit Singh, Neha Kakkar, Meet Bros<br>Movie: Raabta<br>Released: 2017<br>Composer(s): : Sohrabbudin (Original); Sourav Roy (Revamped).<br>Genre: Dance music"
-,"Artist: Badshah<br>Released: 2017<br>Nominations: Mirchi Music Awards for Best Song Engineer - Recording and Mixing"
-,"Artist: KK<br>Movie: Shab<br>Released: 2017"
-,"Artist: Arijit Singh<br>Movie: Shab<br>Released: 2017"
-,"Artists: Arijit Singh, Shashaa Tirupati<br>Movie: Half Girlfriend<br>Released: 2017<br>Written: 2001 (lyrics)<br>Lyricist(s): Manoj Muntashir<br>Composer(s): Mithoon"];
-
-var bool=[];
-for(var i=0; i<sd.length; i++)
-	bool[i]=false;
-
-var icon=["images\\\\1.jpg",
-"images\\\\2.jpg",
-"images\\\\3.jpg",
-"images\\\\4.jpg",
-"images\\\\5.jpg",
-"images\\\\6.jpg",
-"images\\\\7.jpg",
-"images\\\\8.jpg",
-"images\\\\9.jpg",
-"images\\\\10.jpg"];
-
-var mood=[["1","2","3"],["4","5"],["6","7","8"],["9","10"]];
-var mmm=["1.png","1.png","1.png","2.png","2.png","3.png","3.png","3.png","4.png","4.png"];
+//path of (images of songs) folder of each expresion
+var icon=["images\\\\Angry\\\\",
+"images\\\\Happy\\\\",
+"images\\\\Sad\\\\",
+"images\\\\Neutral\\\\"];
 
 
-var songs=new Array(icon.length);
-for (var i = 0; i<icon.length; i++) {
-	songs[i]=new Array(4);
-	songs[i][0]=path[i];
-	songs[i][1]=sd[i];
-	songs[i][2]=icon[i];
-	songs[i][3]=mmm[i];
-	console.log(songs[i][0]);
-	console.log(songs[i][1]);
-	console.log(songs[i][2]);
-}
-
-
-function moody(val){
-	var index=Math.random()*mood[val].length;
-	index=parseInt(index);pa
-	var pa=songs[mood[val][index]-1][0];
-	console.log()
-	document.getElementById("sname").innerHTML=sname[mood[val][index]-1];
-	document.getElementById("stheme").innerHTML='<div id="pic" style=\'background-image: url(\"'+songs[mood[val][index]-1][2]+'\");\'><div id="data"><br><br>'+songs[mood[val][index]-1][1]+'</div>';
-	document.getElementById("sel").src= pa;
-	document.getElementById("main_slider").load();
-	document.getElementById("main_slider").play();
-	document.getElementById("emoji").style.backgroundImage="url('"+songs[mood[val][index]-1][3]+"')";
-	songrun=true;
-}
-
-function callGetTime(){
-	let v =getTime();
-}
 
 function getTime() {
-	
-	eel.getEmotion()(function(value){
-		console.log(value);
-	if(value=="Angry")
-		moody(0);
-	else if(value=="Happy")
-		moody(1);
-	else if(value=="Sad")
-		moody(2);
-	else
-		moody(3);
-})
+	eel.geti()(function(i){
+	eel.playMusic()(function(songname){
+		console.log(i)
+		console.log(songname)
+		document.getElementById("sname").innerHTML=songname;
+		document.getElementById("emoji").style.backgroundImage="url('"+(i+1)+".png')";
+		document.getElementById("sel").src= folderspath[i]+songname+".mp3";
+		document.getElementById("main_slider").load();
+		document.getElementById("main_slider").play();
+		document.getElementById("spic").style.backgroundImage="url('"+icon[i]+songname+".png')";
+		})
+	})
 }
